4a2e6ba994b1e54f4cc20338b59ec463d3fe773b
tleytek <lucidmat94@gmail.com>
2018-07-19 14:55:17 -0400

    Functionality Completegit add .

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,6 +2,18 @@ $(document).ready(function() {
   console.log("ready!");
 
   var topics = ["Hamburger", "Pizza", "Soda", "Ice - Cream"];
+  populate(topics);
+
+  $("#submit-food").on("click", function(event) {
+    event.preventDefault();
+    var inputFood = $("#food-input")
+      .val()
+      .trim();
+    topics.push(inputFood);
+    console.log(topics);
+    populate(topics);
+  });
+
   function populate(topics) {
     //I had to cheat here.
     $("#topics").empty();
@@ -15,15 +27,19 @@ $(document).ready(function() {
       );
     }
   }
-  populate(topics);
-
-  $(".food").on("click", function() {
+  //OMG!!!!! I don't know exactly why this has to be written like this:
+  // $('#selector-to-thing-that-exists').on('click', '.thing-that-will-be-added-later', function() {
+  // alert('Do stuff here!');
+  // });
+  // but I spent a good 2 hours trying to figure out why my images weren't showing up AFTER I added a new topic, I don't know why I didn't just google why after my first 10 minutes.
+  //Now my code is a bit more messy thanks to all the moving around, deleting, and adding of lines.
+  $("#topics").on("click", ".food", function() {
     var food = $(this).attr("data-food");
     var queryURL =
       "https://api.giphy.com/v1/gifs/search?q=" +
       food +
       "&api_key=x9sVAM4jum2qxvuJZC9Ho7iHNNYteQn9&limit=10";
-
+    console.log(food);
     //Ajax GET Request
     $.ajax({
       url: queryURL,
@@ -35,13 +51,9 @@ $(document).ready(function() {
         for (var i = 0; i < results.length; i++) {
           if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
             var gifDiv = $("<div>");
-
             var rating = results[i].rating;
-
             var p = $("<p>").text("Rating: " + rating);
-
             var foodImage = $("<img>");
-
             //Found out how to add multiple attributes, but I couldn't have '-' in any of the attribute names.
             foodImage.attr({
               src: results[i].images.fixed_height_still.url,
@@ -50,17 +62,14 @@ $(document).ready(function() {
               imgAnimate: results[i].images.fixed_height.url,
               class: "gif"
             });
-
             gifDiv.append(p);
             gifDiv.append(foodImage);
-
             $("#gifs").prepend(gifDiv);
           }
         }
         // OK so I found out that if you have this outside of the ajax promise then it won't work? Why is that?
         $("img").on("click", function() {
           var state = $(this).attr("imgState");
-
           if (state === "still") {
             $(this).attr({
               src: $(this).attr("imgAnimate"),
@@ -74,13 +83,6 @@ $(document).ready(function() {
             });
             console.log("I'm frozen");
           }
-        });
-        $("#submit-food").on("click", function() {
-          var inputFood = $("#food-input")
-            .val()
-            .trim();
-          topics.push(inputFood);
-          populate(topics);
         });
       })
       .catch(function(err) {
