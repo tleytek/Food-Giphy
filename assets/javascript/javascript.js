64d2525a6e29bbb7b00f361fd9e2d19d4cd77f9d
tleytek <lucidmat94@gmail.com>
2018-07-19 13:35:37 -0400

    I don't understand why after I add a new food, ajax no longer works and images won't load. It works perfectly until I add a food

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
   console.log("ready!");
 
-  $("button").on("click", function() {
-    var food = $(this).attr("data-food");
+  var topics = ["Hamburger", "Pizza", "Soda", "Ice - Cream"];
+  function populate(topics) {
+    //I had to cheat here.
+    $("#topics").empty();
+    for (i = 0; i < topics.length; i++) {
+      $("#topics").append(
+        "<button data-food='" +
+          topics[i] +
+          "' class='btn food'>" +
+          topics[i] +
+          "</button>"
+      );
+    }
+  }
+  populate(topics);
 
+  $(".food").on("click", function() {
+    var food = $(this).attr("data-food");
     var queryURL =
       "https://api.giphy.com/v1/gifs/search?q=" +
       food +
@@ -13,63 +28,63 @@ $(document).ready(function() {
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response) {
-      var results = response.data;
-      console.log(results);
-      for (var i = 0; i < results.length; i++) {
-        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-          var gifDiv = $("<div class='item'>");
+    })
+      .then(function(response) {
+        var results = response.data;
+        console.log(results);
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+            var gifDiv = $("<div>");
 
-          var rating = results[i].rating;
+            var rating = results[i].rating;
 
-          var p = $("<p>").text("Rating: " + rating);
+            var p = $("<p>").text("Rating: " + rating);
 
-          var foodImage = $("<img>");
+            var foodImage = $("<img>");
 
-          //Found out how to add multiple attributes, but I couldn't have '-' in any of the attribute names.
-          foodImage.attr({
-            src: results[i].images.fixed_height_still.url,
-            imgState: "still",
-            imgStill: results[i].images.fixed_height_still.url,
-            imgAnimate: results[i].images.fixed_height.url,
-            class: "gif"
-          });
+            //Found out how to add multiple attributes, but I couldn't have '-' in any of the attribute names.
+            foodImage.attr({
+              src: results[i].images.fixed_height_still.url,
+              imgState: "still",
+              imgStill: results[i].images.fixed_height_still.url,
+              imgAnimate: results[i].images.fixed_height.url,
+              class: "gif"
+            });
 
-          gifDiv.append(p);
-          gifDiv.append(foodImage);
+            gifDiv.append(p);
+            gifDiv.append(foodImage);
 
-          $("#gifs").prepend(gifDiv);
+            $("#gifs").prepend(gifDiv);
+          }
         }
-      }
+        // OK so I found out that if you have this outside of the ajax promise then it won't work? Why is that?
+        $("img").on("click", function() {
+          var state = $(this).attr("imgState");
 
-      // OK so I found out that if you have this outside of the ajax promise then it won't work? Why is that?
-      $("img").on("click", function() {
-        var state = $(this).attr("imgState");
-
-        if (state === "still") {
-          $(this).attr("src", $(this).attr("imgAnimate"));
-          $(this).attr("imgState", "animate");
-          console.log("I'm animating");
-        } else {
-          $(this).attr("src", $(this).attr("imgStill"));
-          $(this).attr("imgState", "still");
-          console.log("I'm frozen");
-        }
+          if (state === "still") {
+            $(this).attr({
+              src: $(this).attr("imgAnimate"),
+              imgState: "animate"
+            });
+            console.log("I'm animating");
+          } else {
+            $(this).attr({
+              src: $(this).attr("imgStill"),
+              imgState: "still"
+            });
+            console.log("I'm frozen");
+          }
+        });
+        $("#submit-food").on("click", function() {
+          var inputFood = $("#food-input")
+            .val()
+            .trim();
+          topics.push(inputFood);
+          populate(topics);
+        });
+      })
+      .catch(function(err) {
+        console.log(err);
       });
-    });
   });
-
-  // OK so I found out that if you have this outside of your ajax promise then it won't work?
-  // $("img").on("click", function() {
-  //   var state = $(this).attr("imgState");
-
-  //   if (state === "still") {
-  //     $(this).attr("src", $(this).attr("imgAnimate"));
-  //     $(this).attr("imgState", "animate");
-  //     console.log("Im pressed");
-  //   } else {
-  //     $(this).attr("src", $(this).attr("imgStill"));
-  //     $(this).attr("imgState", "still");
-  //   }
-  // });
 });
